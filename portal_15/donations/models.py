83047ee3f5d0d37db5fb8b0131a929e63f6544d7
alejo-castellanos <alejandro.castellanos@grupor5.com>
2025-03-28 13:34:11 -0500

    feat: new endpoint to create donation

--- portal_15/donations/models.py
+++ portal_15/donations/models.py
@@ -5,7 +5,7 @@ class Donation(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=255)
     phone_number = models.CharField(max_length=20)
-    email = models.EmailField(unique=True)
+    email = models.EmailField(max_length=250)
     donation_amount = models.DecimalField(max_digits=10, decimal_places=2)
     is_public = models.BooleanField(default=False)  # True = Public, False = Private
     is_approved = models.BooleanField(default=False)  # True = Public, False = Private
