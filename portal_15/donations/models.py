78b0de5a34c90449a7edade26fe6bbed77b56b9e
alejo-castellanos <alejandro.castellanos@grupor5.com>
2025-03-28 13:24:23 -0500

    feat: model donations and base project

---
+++ portal_15/donations/models.py
@@ -0,0 +1,14 @@
+import uuid
+from django.db import models
+
+class Donation(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=255)
+    phone_number = models.CharField(max_length=20)
+    email = models.EmailField(unique=True)
+    donation_amount = models.DecimalField(max_digits=10, decimal_places=2)
+    is_public = models.BooleanField(default=False)  # True = Public, False = Private
+    is_approved = models.BooleanField(default=False)  # True = Public, False = Private
+
+    def __str__(self):
+        return f"{self.name} - ${self.donation_amount}"
